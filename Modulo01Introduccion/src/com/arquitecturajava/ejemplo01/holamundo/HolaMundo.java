9f12b12d61f2b9a2ddc926dba13e02bdbe1dfc7b
Marcelo <Marcelo@DESKTOP-JOHEJIQ>
2024-11-05 23:40:47 -0300

    Actualizando mi hola mundo

--- Modulo01Introduccion/src/com/arquitecturajava/ejemplo01/holamundo/HolaMundo.java
+++ Modulo01Introduccion/src/com/arquitecturajava/ejemplo01/holamundo/HolaMundo.java
@@ -3,7 +3,7 @@ package com.arquitecturajava.ejemplo01.holamundo;
 public class HolaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo desde Java 2024!, ahora si vamos con todo y ahora usando git");
+		System.out.println("Hola Mundo desde Java 2024!, ahora si vamos con todo");
 	}
 
 }
