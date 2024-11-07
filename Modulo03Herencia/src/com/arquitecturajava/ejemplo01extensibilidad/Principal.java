6629667c8783e7281c43968b3df2f13163475964
ADMCarlosA <ADMCarlosA@EQC106-MAQ>
2024-11-07 12:10:33 -0300

    Actualizando el modulo 3 desde soluex

--- Modulo03Herencia/src/com/arquitecturajava/ejemplo01extensibilidad/Principal.java
+++ Modulo03Herencia/src/com/arquitecturajava/ejemplo01extensibilidad/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	
 	public static void main(String [] args) {
 		Deportista d1 = new Deportista();
-		d1.setNombre("Pedro");
+		d1.setNombre("Juan");
 		d1.setDeporte("Tenis");
 		
 		System.out.println(d1.getNombre());
