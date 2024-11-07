200bef5296b6c33f69b16ee00c2b1f03d87e444a
ADMCarlosA <ADMCarlosA@EQC106-MAQ>
2024-11-07 11:39:31 -0300

    actualiza el hola mundo soluex

--- Modulo01Introduccion/src/com/arquitecturajava/ejemplo01/holamundo/HolaMundo.java
+++ Modulo01Introduccion/src/com/arquitecturajava/ejemplo01/holamundo/HolaMundo.java
@@ -3,7 +3,7 @@ package com.arquitecturajava.ejemplo01.holamundo;
 public class HolaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo desde Java 2024!, ahora si vamos con todo ahora si");
+		System.out.println("Hola Mundo desde Java 2024!, actualizado desde Soluex");
 	}
 
 }
